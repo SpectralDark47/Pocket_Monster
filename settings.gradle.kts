5c7faa82212335e119781273a4c202d9155c2f9f
Wardc0714 <wardc0714@outlook.com>
2024-02-26 16:24:54 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Who's that Pocket Monster"
+include(":app")
+ 
