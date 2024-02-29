8c00ec96ddd85f9478e8e29dbdb3733153a8bf9f
Colton Ward <wardc0714@outlook.com>
2024-02-28 20:35:15 -0600

    post eric office hours

--- app/src/main/java/com/example/whosthatpocketmonster/MainActivity.kt
+++ app/src/main/java/com/example/whosthatpocketmonster/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.whosthatpocketmonster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun submitOnClick(view: View){
+        val button = findViewById<Button>(R.id.submit_btn)
+        val image = findViewById<ImageView>(R.id.imageView)
+        image.setImageResource(R.drawable.alakazam_silh)
+        if (button.getText() == "Submit"){
+            button.setText("Next")
+        } else if (button.getText() == "Next"){
+            button.setText("Submit")
+        }
+
     }
 
 }
